41ec02c66e7aeddbee3d055305608177fca4286a
marina.shilo <marina.shilo@degree53.com>
2023-01-25 17:25:59 +0300

    feat: add prettier

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -2,7 +2,7 @@ export enum ErrorMessages {
 	USER_ERROR = 'User not found',
 	PROFILE_ERROR = 'Profile not found',
 	POST_ERROR = 'Post not found',
-	MEMBER_TYPE_ERROR = 'Member-type not found',
+	MEMBER_TYPE_ERROR = 'MemberType not found',
 	BAD_REQUEST = 'Bad Request',
 	NOT_FOUND = 'Not Found',
 };
