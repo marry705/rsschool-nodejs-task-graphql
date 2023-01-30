8c20db8480d77516356886785244f69df3108209
marina.shilo <marina.shilo@degree53.com>
2023-01-30 12:09:26 +0300

    feat: add depth-limit validations

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,16 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
-import { graphql, GraphQLSchema } from 'graphql';
+import {
+	DocumentNode,
+	graphql,
+	GraphQLSchema,
+	parse,
+	Source,
+	validate,
+} from 'graphql';
 import { RootMutation } from './mutations';
 import { RootQuery } from './query';
 import { graphqlBodySchema } from './schema';
+import * as depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
 	fastify
@@ -14,11 +22,27 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
 				body: graphqlBodySchema,
 			},
 		},
-		async function (request) {
+		async function (request, reply) {
 			const schema: GraphQLSchema = new GraphQLSchema({
 				query: RootQuery,
 				mutation: RootMutation,
 			});
+
+			let document: DocumentNode;
+
+			try {
+				document = parse(
+					new Source(String(request.body.query), 'GraphQL request')
+				);
+			} catch (error) {
+				return fastify.httpErrors.badRequest((error as Error).message);
+			}
+
+			const errors = validate(schema, document, [depthLimit(6)]);
+
+			if (errors.length > 0) {
+				return reply.send({ data: null, errors: errors });
+			}
 
 			return await graphql({
 				schema: schema,
